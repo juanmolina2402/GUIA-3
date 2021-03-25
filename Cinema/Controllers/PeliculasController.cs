c0ae36cf31bd2caea47abd07640e22e643396288
juanmolina2402 <1cajuanmolina@gmail.com>
2021-03-24 23:23:33 -0600

    literal a

--- Cinema/Controllers/PeliculasController.cs
+++ Cinema/Controllers/PeliculasController.cs
@@ -25,46 +25,82 @@ namespace Cinema.Controllers
             //return RedirectToAction("Index", "Home", new { pag = 1, OrdenadoPor = "nombre" });
         }
 
-        public ActionResult Editar(int id)
-        {
-            return Content("Id: " + id);
-        }
+        //public ActionResult Editar(int id)
+        //{
+        //    return Content("Id: " + id);
+        //}
 
-        //Get Peliculas
+        ////Get Peliculas
+        //public ActionResult Index(int? pagIndex, String ordenadoPor)
+        //{
+        //    if (!pagIndex.HasValue)
+        //        pagIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(ordenadoPor))
+        //        ordenadoPor = "Nombre";
 
-        public ActionResult Index(int? pagIndex, String ordenadoPor)
-        {
-            if (!pagIndex.HasValue)
-                pagIndex = 1;
-            if (String.IsNullOrWhiteSpace(ordenadoPor))
-                ordenadoPor = "Nombre";
+        //    return Content(String.Format("Indice de Página={0}&Ordenado por ={1}", pagIndex, ordenadoPor));
+        //}
 
-            return Content(String.Format("Indice de Página={0}&Ordenado por ={1}", pagIndex, ordenadoPor));
-        }
+        ////GET /Peliculas/PorFechaLanzamiento/year/mes
+        //public ActionResult PorFechaLanzamiento(int year, int mes)
+        //{
+        //    return Content(year + "/" + mes);
+        //}
 
-        //GET /Peliculas/PorFechaLanzamiento/year/mes
-        public ActionResult PorFechaLanzamiento(int year, int mes)
-        {
-            return Content(year + "/" + mes);
-        }
 
-        ////EJERCICIO GUIA 3
 
+        ///////////////////////////EJERCICIO GUIA 3 //////////////////////////////////////////////////////////////////////////////
         // GET: Peliculas/PorOrden
-        public ActionResult PorOrden()
+        public ActionResult PorOrden(Int32 opc=0)
         {
-            var pelicula = new Pelicula()
+            List<Pelicula> listaPelicula = new List<Pelicula>()
             {
-                Id = 2,
-                Nombre = "Shrek",
-                Genero = "Fantasía",
-                AnioDeLanzamiento = "2001",
-                Clasificacion = "Todas las edades"
+                new Pelicula()
+                {
+                    Id = 1,
+                    Nombre = "Rapidos y furiosos",
+                    Genero = "Accion",
+                    AnioDeLanzamiento = "2001",
+                    Clasificacion = "+11"
+                },
+
+                new Pelicula()
+                {
+                    Id = 2,
+                    Nombre = "El rey león",
+                    Genero = "Animacion",
+                    AnioDeLanzamiento = "2000",
+                    Clasificacion = "Todo público"
+                },
+
+                new Pelicula()
+                {
+                    Id = 3,
+                    Nombre = "Los Vengadores",
+                    Genero = "Ciencia ficción",
+                    AnioDeLanzamiento = "2012",
+                    Clasificacion = "Todo público"
+                },
+
+                new Pelicula()
+                {
+                    Id = 4,
+                    Nombre = "La liga de la justicia",
+                    Genero = "Ciencia ficción",
+                    AnioDeLanzamiento = "2021",
+                    Clasificacion = "Todo público"
+                },
             };
-            return View(pelicula);
-            //return Content("Hola Mundo!");
-            //return HttpNotFound();
-            //return RedirectToAction("Index", "Home", new { pag = 1, OrdenadoPor = "nombre" });
+            
+            if(opc == 0)
+            {
+                return View(listaPelicula);
+            }
+            else { 
+                List<Pelicula> listaOrdenadaNombre = listaPelicula.OrderBy(pNombre => pNombre.Nombre).ToList();
+                return View(listaOrdenadaNombre);
+            }
+
         }
 
     }
